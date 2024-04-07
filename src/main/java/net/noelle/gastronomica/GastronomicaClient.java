7f04e157040d65b27e4d9ae8e430c7e3f7446670
PainIsHere58 <ruthra2016.1@gmail.com>
2024-04-07 15:57:14 -0300

    Initial commit

---
+++ src/main/java/net/noelle/gastronomica/GastronomicaClient.java
@@ -0,0 +1,10 @@
+package net.noelle.gastronomica;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GastronomicaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/noelle/gastronomica/GastronomicaDataGenerator.java
@@ -0,0 +1,11 @@
+package net.noelle.gastronomica;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GastronomicaDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

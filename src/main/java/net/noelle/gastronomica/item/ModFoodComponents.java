2146d8155abf1059e70db4e549a2003791318d6e
PainIsHere58 <ruthra2016.1@gmail.com>
2024-04-07 23:28:40 -0300

    nothing much

--- src/main/java/net/noelle/gastronomica/item/ModFoodComponents.java
+++ src/main/java/net/noelle/gastronomica/item/ModFoodComponents.java
@@ -7,7 +7,8 @@ import net.minecraft.item.FoodComponent;
 public class ModFoodComponents {
     public static final FoodComponent ROCK_HARD_STEW =
             new FoodComponent.Builder().hunger(2).saturationModifier(0.25f)
-                    .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 2), 1.0f)
+                    .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 2), 1.0f)
+                    .statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 200, 3), 1.0f)
                     .build();
     public static final FoodComponent BAKED_APPLE =
             new FoodComponent.Builder().hunger(6).saturationModifier(0.5f).build();
--- src/main/java/net/noelle/gastronomica/item/ModItems.java
+++ src/main/java/net/noelle/gastronomica/item/ModItems.java
@@ -1,10 +1,7 @@
 package net.noelle.gastronomica.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -14,10 +11,11 @@ public class ModItems {
 
     public static final Item BAKED_APPLE = registerItem("baked_apple", new Item(new FabricItemSettings()
             .food(ModFoodComponents.BAKED_APPLE)));
-    public static final Item SKEWER = registerItem("skewer", new Item(new FabricItemSettings()));
+    public static final Item SKEWER = registerItem("skewer", new Item(new FabricItemSettings()
+            .maxCount(16)));
 
     public static final Item ROCK_HARD_STEW = registerItem("rock_hard_stew", new Item(new FabricItemSettings()
-            .food(ModFoodComponents.ROCK_HARD_STEW)));
+            .maxCount(1).food(ModFoodComponents.ROCK_HARD_STEW)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Gastronomica.MOD_ID, name), item);
